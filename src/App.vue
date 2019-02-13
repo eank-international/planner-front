cb76df4ee764368576641168bb2d9032e2c56d41
Olgierd Kasprowicz <olixem@gmail.com>
2019-02-13 22:38:34 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Eank International is the best!"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default 
+{
+    name: 'app',
+    components:
+    {
+        HelloWorld,
+    },
+}
+</script>
