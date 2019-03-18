c51208f3908180666eb71b0cd14dfe1d34599cce
KittyMiau <verisa95@gmail.com>
2019-03-18 23:13:49 +0100

    Add calendar template

--- src/components/CalendarView.vue
+++ src/components/CalendarView.vue
@@ -1,7 +1,38 @@
 <template>
-    <div>
-        {{ msg }}
-    </div>
+    <div class="container"> 
+          <div></div>
+          <div>Poniedziałek</div>
+          <div>Wtorek</div>
+          <div>Środa</div>
+          <div>Czwartek</div>
+          <div>Piątek</div>
+          <div>Sobota</div>
+          <div>Niedziela</div>
+          <div>Śniadanie</div>
+          <div>jajecznica</div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div>Obiad</div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div>Kolacja</div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>         
+      </div>
 </template>
 
 <script>
@@ -16,4 +47,19 @@ export default
 </script>
 
 <style scoped>
+.container{
+    display: grid;
+    grid-template-columns: repeat(8,auto);
+    grid-template-rows: repeat(4, auto);
+    border: 1px solid black;
+    
+}
+
+.container div{
+    border:1px solid black;
+    padding:20px;
+    text-align: center;
+}
+
+
 </style>
